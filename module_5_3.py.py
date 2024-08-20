43aba78b4bede9817755415456d19eb77929a7bc
Sunktum0 <vlad99-2010@yandex.ru>
2024-08-20 16:25:19 +0300

    Add files via upload

---
+++ module_5_3.py.py
@@ -0,0 +1,65 @@
+
+
+class house:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le_(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        return self
+
+    def __radd__(self, value):
+        return self
+
+
+
+h1 = house('Olympus', 10)
+h2 = house('Viktoria', 20)
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
